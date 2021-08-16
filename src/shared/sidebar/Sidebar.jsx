646e8d1c1c6476e3c51238f364d540ef56877583
niranjanmaradapu <85561667+niranjanmaradapu@users.noreply.github.com>
2021-08-16 13:46:17 +0530

    Add files via upload

--- src/shared/sidebar/Sidebar.jsx
+++ src/shared/sidebar/Sidebar.jsx
@@ -10,10 +10,32 @@ import returnslip from "../../assets/images/return_slip.svg";
 import addcustomer from "../../assets/images/create_customer.svg";
 import tagcustomer from "../../assets/images/tag_customer.svg";
 import dayclose from "../../assets/images/close.svg";
+import cashmemo from "../../assets/images/cash_memo.svg";
+import bigarrow from "../../assets/images/arrow_big_left.svg";
+import arrow_sm from "../../assets/images/arrow_sm.svg";
+import notes from "../../assets/images/notes.svg";
+import notesadd from "../../assets/images/notes_add.svg";
+import Transfers from "../../assets/images/transfer.svg";
+import stock from "../../assets/images/stock.svg";
+import Rebarcoading from "../../assets/images/rebarcoading.svg";
+import audits from "../../assets/images/audit.svg";
+import ecommerce from "../../assets/images/ecommerce.svg";
+import masters from "../../assets/images/management.svg";
+import pramotions from "../../assets/images/pramotions.svg";
+import loyalty from "../../assets/images/loyalty_promo.svg";
+import users from "../../assets/images/user_privileges.svg";
+import reports from "../../assets/images/chart.svg";
+import r_brand from "../../assets/images/r_brand.svg";
+import Hsn from "../../assets/images/hsn.svg";
 
 class Sidebar extends Component {
     
-     
+     constructor(props){
+         super(props)
+         this.state={
+            slideValue:"slide-in"
+         }
+     }
     newSale = () => {
         this.props.history.push("newsale");
     }
@@ -23,6 +45,13 @@ class Sidebar extends Component {
     promoExchange = () => {
         this.props.history.push("promoitemexchange");
     }
+    login = () => {
+        this.state.slideValue="slide-in"
+    }
+    slideOut = () => {
+        document.body.classList.add('slide');
+        // document.body.classList.add('slide');
+    }
     render() {
         return (
             <div className="">
@@ -31,7 +60,7 @@ class Sidebar extends Component {
                 </div>
             <div className="sidebar-menu">
                 <ul>
-                    <li>
+                    <li onClick={this.login}>
                         <img src={list} /><span>All Modules</span> <img src={arrow} />
                     </li>
                      <li className="active" onClick={this.createDeliverySlip}>
@@ -57,8 +86,102 @@ class Sidebar extends Component {
                     </li>
                 </ul>
             </div>
+            <div className={this.state.slideValue}>
+                <div className="row">
+                    <div className="col-6 slide-head">
+                        <h6 className="text-blue fs-20 font-bold">List Of Modules</h6>
+                    </div>
+                    <div className="col-6 text-right slide-head">
+                        <button className="btn-transparent" onClick={this.slideOut} type="button"><img src={bigarrow}/></button>
+                    </div>
+                </div>
+                <div className="slide-body">
+                    <ul>
+                        <li>
+                                <img className="arrow-align"src={arrow_sm}/>                           
+                                <img src={cashmemo} />
+                                <h6>Cash Memo & Delivery Slips</h6>
+                        </li>
                
+                        <li>
+                                <img className="arrow-align"src={arrow_sm}/>  
+                                <img src={notes} />
+                                <h6>Customer Credit Notes</h6>
+                        </li>
+                        <li>
+                                <img className="arrow-align"src={arrow_sm}/>  
+                                <img src={notesadd} />
+                                <h6>Customer Debit Notes</h6>
+                        </li>
+                        <li>
+                                <img className="arrow-align"src={arrow_sm}/>  
+                                <img src={Transfers} />
+                                <h6>Transfers</h6>
+                        </li>
+                        <li>
+                                <img className="arrow-align"src={arrow_sm}/>                           
+                                <img src={stock} />
+                                <h6>Stock</h6>
+                        </li>
+               
+                        <li>
+                                <img className="arrow-align"src={arrow_sm}/>  
+                                <img src={Rebarcoading} />
+                                <h6>Rebarcoading</h6>
+                        </li>
+                        <li>
+                                <img className="arrow-align"src={arrow_sm}/>  
+                                <img src={audits} />
+                                <h6>Stock audits</h6>
+                        </li>
+                        <li>
+                                <img className="arrow-align"src={arrow_sm}/>  
+                                <img src={ecommerce} />
+                                <h6>Ecommerce</h6>
+                        </li>
+                        <li>
+                                <img className="arrow-align"src={arrow_sm}/>  
+                                <img src={users} />
+                                <h6>Users & privileges</h6>
+                        </li>
+                        <li>
+                                <img className="arrow-align"src={arrow_sm}/>                           
+                                <img src={masters} />
+                                <h6>Manage Masters</h6>
+                        </li>
+               
+                        <li>
+                                <img className="arrow-align"src={arrow_sm}/>  
+                                <img src={pramotions} />
+                                <h6>Manage Pramotions</h6>
+                        </li>
+                        <li>
+                                <img className="arrow-align"src={arrow_sm}/>  
+                                <img src={loyalty} />
+                                <h6>Loyalty promo Settings</h6>
+                        </li>
+                        <li>
+                                <img className="arrow-align"src={arrow_sm}/>  
+                                <img src={reports} />
+                                <h6>Reports</h6>
+                        </li>
+                        <li>
+                                <img className="arrow-align"src={arrow_sm}/>  
+                                <img src={r_brand} />
+                                <h6>R - Brand</h6>
+                        </li>
+                        <li>
+                                <img className="arrow-align"src={arrow_sm}/>  
+                                <img src={Hsn} />
+                                <h6>Text & HSN Master</h6>
+                        </li>
+                    </ul>
+                </div>
             </div>
+            </div>
+            
+
+
         )
     }
 }

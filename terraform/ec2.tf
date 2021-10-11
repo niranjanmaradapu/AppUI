9a9d09ec5a97f4f46f6cc4c8d2d0084e3e54459c
ramakri4u <ramakri4u@yahoo.com>
2021-10-11 17:11:22 +0530

    Added the connection block in remote-exec

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -32,10 +32,45 @@ data "aws_ami" "ubuntu" {
   owners = ["099720109477"] # Canonical
 }
 
+//Create Security Group
+resource "aws_security_group" "rules" {
+  name        = "allow_react_ssh"
+  description = "Allow React and SSH inbound traffic"
+  revoke_rules_on_delete = "true"
+  ingress {
+    description = "React from VPC"
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress{
+    description = "SSH for VPC"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "security_group"
+  }
+}
+
 resource "aws_instance" "appui" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
   key_name = "myKey"
+  security_groups = [ "${aws_security_group.rules.name}" ] 
 
   tags = {
     Name = "APP-UI"

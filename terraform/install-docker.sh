b37906cd9dd552d62754e943370dc09b0cfc1b55
ramakri4u <ramakri4u@yahoo.com>
2021-10-13 09:44:41 +0530

    added auto approve from install in linux

--- terraform/install-docker.sh
+++ terraform/install-docker.sh
@@ -1,8 +1,8 @@
-sudo apt-get remove docker docker-engine docker.io containerd runc
+sudo apt-get remove docker docker-engine docker.io containerd runc -y
 
-sudo apt-get update
+sudo apt-get update -y
 
-sudo apt-get install \
+sudo apt-get -y install \
     apt-transport-https \
     ca-certificates \
     curl \
@@ -15,9 +15,9 @@ echo \
   "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-sudo apt-get update
+sudo apt-get update -y
 
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io
 
 
 ## docker-compose install:

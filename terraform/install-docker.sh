43db68626714d826652201170af8b2a992ac9a34
ramakri4u <ramakri4u@yahoo.com>
2021-10-19 09:40:17 +0530

    added the environment variab;es

--- terraform/install-docker.sh
+++ terraform/install-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
-AWS_ACCESS_KEY = $1
-AWS_SECRET_KEY = $2
+AWS_ACCESS_KEY_ID=$1
+AWS_SECRET_ACCESS_KEY=$2
 
 sudo apt-get remove docker docker-engine docker.io containerd runc -y
 
@@ -36,8 +36,8 @@ sudo chmod +x /usr/local/bin/docker-compose
 sudo apt install awscli -y 
 
 aws configure set default.region ap-south-1
-aws configure set aws_access_key_id   $AWS_ACCESS_KEY
-aws configure set aws_secret_access_key $AWS_SECRET_KEY
+aws configure set aws_access_key_id   "$AWS_ACCESS_KEY_ID"
+aws configure set aws_secret_access_key "$AWS_SECRET_ACCESS_KEY"
 
 aws ecr get-login-password --region ap-south-1 | docker login --username AWS --password-stdin 881289302514.dkr.ecr.ap-south-1.amazonaws.com
 docker pull 881289302514.dkr.ecr.ap-south-1.amazonaws.com/app-ui:latest

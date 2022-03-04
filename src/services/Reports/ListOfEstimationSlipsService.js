da96f3c9b453dad3806f22c46fc046d9b68d50c5
NeelimaSharon123 <neelima.boddu@otsi.co.in>
2022-03-04 17:31:39 +0530

    added the recent code

---
+++ src/services/Reports/ListOfEstimationSlipsService.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import {ESTIMATIONSLIP_LIST_URL} from '../../commonUtils/ApiConstants';
+import { BASE_URL } from "../../commonUtils/Base";
+
+class ListOfEstimationSlipsService{
+    getEstimationSlips(data){
+        return axios.post(BASE_URL+ESTIMATIONSLIP_LIST_URL.estimationslipsList,data);
+    }
+}
+
+
+export default new ListOfEstimationSlipsService()

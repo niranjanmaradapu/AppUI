cfb0b55b97e2adad50920ea27b3c302d18549d30
muralichittoji <murali.chittoji@otsi.co.in>
2022-06-09 11:45:44 +0530

    BArcode Print

--- src/commonUtils/checkPrinter.js
+++ src/commonUtils/checkPrinter.js
@@ -1,90 +1,58 @@
 import React, { useRef, useState } from "react";
+// import {BrowserPrint} from 'browserprint'
+const PrintBarcode = (type,object) => {
+  console.log(type);
+  console.log(object);
+  var devices = []
+  let selected_device =null
+  let devicename =""
+  // let count =object.qty;
+  let count = 1;
+  let barcode=object.barcode;
+  let barcodeName = object.name;
+  let barcodePrice = object.itemMrp;
+  let barcodeCategory = object.category;
 
-const printerReciept = () => {
-  // 192.168.1.13  HOME
-  // 10.80.2.50 OFC
-   const printSucess = "printed...."
-     let printer;
-    let prn = printer.current;
-    if (!prn) {
-      alert("Not connected to printer");
-      return;
-    }
-    // var receiptString =
-    // "======================\n" +
-    // "|       Invoice      |\n" +
-    // "======================\n" +
-    // "Item             Price\n" +
-    // "----------------------\n" +
-    // "shirts           10.40\n" +
-    // "T-shirts         20.60\n" +
-    // "paste            10.00\n" +
-    // "Idlyrava         15.00\n" +
-    // "pants            10.00\n" +
-    // "bendi            25.00\n" +
-    // "brinja           10.00\n" +
-    // "tamatoo          45.00\n" +
-    // "shorts           50.00\n" +
-    // "shoes            70.00\n" +
-    // "sandle           30.00\n" +
-    // "fliplops         55.00\n" +
-    // "----------------------\n" +
-    // "Total-----------220.00\n";
-    // prn.addText(receiptString);
-    // prn.addFeedLine(5);
-    // prn.addCut(prn.CUT_FEED);
+  // let  BrowserPrint = {},
+  //Get the default device from the application as a first step. Discovery takes longer to complete.
+  window.BrowserPrint.getDefaultDevice("printer", function (device) {
 
-    // prn.send();
-
-   // *************************PRINT BILL*****************************
-    prn.addTextPosition(212);
-    prn.addTextVPosition(486);
-    prn.addText('EASY RETAIL\n');
-    prn.addTextPosition(240);
-    prn.addText('Invoice\n');
-    prn.addText(' =============================================\n');
-    prn.addTextPosition(3);
-    prn.addBarcode('ESYRETAIL7895574', prn.BARCODE_CODE39, prn.HRI_BELOW, prn.FONT_B, 2, 90);
-    prn.addText(' =============================================\n');
-    prn.addTextPosition(10);
-    prn.addText('CUSTOMER NAME:   Kadali         \n');
-    prn.addTextPosition(10);
-    prn.addText('Mobile       :   8328418471      \n');
-    prn.addText(' =============================================\n');
-    prn.addText(' |                ITEMS LIST                 |\n');
-    prn.addText(' =============================================\n');
-    prn.addText(' S.No   Item     Qty    Type     Disc    Price\n');
-    prn.addText(' ---------------------------------------------\n');
-    prn.addText(' 1.    shirts     2       M      0.0   1850.00\n');
-    prn.addText(' 2.    sarees     4       F      0.0   2550.00\n');
-    prn.addText(' 3.    T-shirts   3       M      0.0   3000.00\n');
-    prn.addText(' 4.    shorts     2       M      0.0    750.00\n');
-    prn.addText(' 5.    pants      1       T      0.0    800.00\n');
-    prn.addText(' 6.    shoes      1       S      0.0   1250.00\n');
-    prn.addText(' 7.    socks      2       M      0.0    250.00\n');
-    prn.addText(' 8.    paste      5       Y      0.0    450.00\n');
-    prn.addText(' 9.    soaps      6       M      0.0    350.00\n');
-    prn.addText(' 10.   brushes    5       F      0.0    800.00\n');
-    prn.addText(' ---------------------------------------------\n');
-    prn.addText(' Total            31             0.0  11230.00\n');
-    prn.addText(' ---------------------------------------------\n');
-    prn.addText(' Tax                                          \n');
-    prn.addText(' ---------------------------------------------\n');
-    prn.addText(' SGST                                   125.00\n');
-    prn.addText(' CGST                                   125.00\n');
-    prn.addText(' IGST                                     0.00\n');
-    prn.addText(' ---------------------------------------------\n');
-    prn.addText(' Net Total                            11230.00\n');
-    prn.addText(' ---------------------------------------------\n');
-    // prn.addSymbol('http://www.google.com/', prn.SYMBOL_QRCODE_MODEL_1, prn.LEVEL_DEFAULT, 9, 8, 350);
-    prn.addText(' ==================THANK YOU===================\n');
-    prn.addCut(prn.CUT_FEED);
-    // *************************PRINT BILL*****************************
+    //Add device to list of devices and to html select element
+    selected_device = device;
+    devices.push(device);
+    // var html_select = document.getElementById("selected_device");
+    // var option = document.createElement("option");
+    devicename = device.name;
    
-    prn.send();
+    // html_select.add(option);
+    //  selected_device.send('^XA^CF0,50^FO150,30^FDEasy Retail^FS^CF0,30^FX Third section with bar code.^BY3,1,100^FO100,80^BC^FD'+barcode+'^FS^FO70,240^GB450,120,3^FS^FO300,240^GB3,120,3^FS^CF0,20^FO100,260^FDName:^FS^FO100,290^FDPrice:^FS^FO100,320^FDCategory:^FS^CF0,20^FO380,260^FD' +barcodeName+'^FS^FO380,290^FD'+barcodePrice+'^FS^FO380,320^FD'+barcodeCategory+'^FS^XZ'  , undefined, null);
+    for(let i=0;i<count;i++){
+     selected_device.send('^XA^CF0,50^FO150,30^FDEasy Retail^FS^CF0,30^FX Third section with bar code.^BY3,1,100^FO100,80^BC^FD'+barcode+'^FS^FO70,240^GB450,120,3^FS^FO300,240^GB3,120,3^FS^CF0,20^FO100,260^FDName: ' +barcodeName+'^FS^CF0,30^FO100,290^FDPrice: '+barcodePrice+'^FS^CF0,20^FO100,325^FDCategory: '+barcodeCategory+'^FS^CF0,60^FO380,260^FDOTSI^FS^XZ'  , undefined, null);
+    }
+    //Discover any other devices available to the application
+    window.BrowserPrint.getLocalDevices(function (device_list) {
+      var printers_available = false;
+      for (var i = 0; i < device_list.length; i++) {
+        //Add device to list of devices and to html select element
+        var device = device_list[i];
+        if (!selected_device || device.uid != selected_device.uid) {
+          devices.push(device);
+          var option = document.createElement("option");
+          option.text = device.name;
+          option.value = device.uid;  
+          printers_available = true
+          // html_select.add(option);
+        }
+      }
+      
 
-    return printSucess
+    }, function () { alert("Error getting local devices") }, "printer");
+
+  }, function (error) {
+    alert(error);
+  })
+
  
 };
 
-export default printerReciept;
+export default PrintBarcode;

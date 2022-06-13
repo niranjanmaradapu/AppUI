645ccd464bce9054b12ab8eebf48c8943ac7b596
Sravani Madaka <sravani.madaka@otsi.com>
2022-06-13 16:14:32 +0530

    short_cut_keys

--- src/components/cashmemo-deliveryslip/CreateDeliverySlip.jsx
+++ src/components/cashmemo-deliveryslip/CreateDeliverySlip.jsx
@@ -16,6 +16,8 @@ import Table from 'react-bootstrap/Table';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Select from "react-select";
+import {  withHotKeys } from "react-hotkeys";
+import Hotkeys from 'react-hot-keys';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from "reactstrap";
 import { withTranslation } from 'react-i18next';
 import { saveDataInIndexDB, getDataFromIndexDB } from '../../utility.js';
@@ -78,6 +80,20 @@ class CeateDeliverySlip extends Component {
     //  this.deleteTableRow = this.deleteTableRow.bind(this);
 
 
+  }
+  onKeyDown(keyName, e, handle) {
+    console.log("test:onKeyDown", keyName, e, handle)
+    // this.setState({
+    //   output: `onKeyDown ${keyName}`,
+    // });
+    if(keyName === 'shift+a'){
+      this.checkPromo();
+
+    }
+    if(keyName === 'shift+z'){
+      this.generateEstimationSlip();
+
+    }
   }
 
 
@@ -719,6 +735,10 @@ class CeateDeliverySlip extends Component {
 
   render() {
     return (
+      <Hotkeys 
+      keyName="shift+a,shift+z" 
+      onKeyDown={this.onKeyDown.bind(this)}
+    >
       <div className="maincontent">
         {/* <h5>Estimation Slip</h5> */}
         {/* <h5> {t("EstimationSlip")}</h5> */}
@@ -902,6 +922,7 @@ class CeateDeliverySlip extends Component {
 
         {/* <ToastContainer /> */}
       </div>
+      </Hotkeys>
     );
   }
 }
